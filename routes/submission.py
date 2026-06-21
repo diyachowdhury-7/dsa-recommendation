c1916795befa637acc8eedf07e2a9c27d2add22d
Shraddha Sidhan <shraddhasidhan@Shraddhas-MacBook-Air.local>
2026-06-21 10:06:40 +0530

    fix: clean up empty lines, fix weak_topic indentation

--- routes/submission.py
+++ routes/submission.py
@@ -1,12 +1,26 @@
-from pydantic import BaseModel
+from fastapi import APIRouter
+from models.schemas.submission import Submission
+from controllers.submission_controller import handle_update_bkt, handle_update_hlr
 
-class Submission(BaseModel):
-    userId: str
-    problemId: str
-    verdict: str
-    testCasesPassed: int
-    totalTestCases: int
-    hintsUsed: int
-    submissionCount: int
-    normalisedScore: float
-    timestamp: float
+router = APIRouter()
+
+@router.post("/update_bkt")
+def update_bkt_endpoint(submission: Submission):
+    updated_mastery, mastered_topics, results = handle_update_bkt(submission)
+    return {
+        "userId": submission.userId,
+        "problemId": submission.problemId,
+        "results": results,
+        "mastered_topics": mastered_topics,
+        "updated_mastery": updated_mastery
+    }
+
+@router.post("/update_hlr")
+def update_hlr_endpoint(submission: Submission):
+    updated_hlr, results = handle_update_hlr(submission)
+    return {
+        "userId": submission.userId,
+        "problemId": submission.problemId,
+        "results": results,
+        "updated_hlr": updated_hlr
+    }
